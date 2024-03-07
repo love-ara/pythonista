bbee66c988a4ed4fd2feb460c66ef7ffd6a31cb8
love-ara <aramideotenaike@gmail.com>
2024-03-07 07:20:19 +0100

    commit

--- src/logistic_func.py
+++ src/logistic_func.py
@@ -1,21 +1,20 @@
-class LogisticFunction:
-    def calculate_payment(self, successful_deliveries):
-        if successful_deliveries <= 0:
-            raise ValueError("Successful deliveries cannot be negative")
-        elif successful_deliveries > 100:
-            raise ValueError("Successful deliveries cannot be more than 100")
+def calculate_payment(successful_deliveries) -> int:
+    if successful_deliveries <= 0:
+        raise ValueError("Successful deliveries cannot be negative")
+    elif successful_deliveries > 100:
+        raise ValueError("Successful deliveries cannot be more than 100")
 
-        base_pay = 5000
+    base_pay = 5000
 
-        if successful_deliveries < 50:
-            wage = successful_deliveries * 160
-        elif 50 <= successful_deliveries < 60:
-            wage = successful_deliveries * 200
-        elif 60 <= successful_deliveries < 70:
-            wage = successful_deliveries * 250
-        else:
-            wage = successful_deliveries * 500
+    if successful_deliveries < 50:
+        wage = successful_deliveries * 160
+    elif 50 <= successful_deliveries < 60:
+        wage = successful_deliveries * 200
+    elif 60 <= successful_deliveries < 70:
+        wage = successful_deliveries * 250
+    else:
+        wage = successful_deliveries * 500
 
-        return wage + base_pay
+    return wage + base_pay
 
 
--- test/test_logistic_func.py
+++ test/test_logistic_func.py
@@ -1,35 +1,33 @@
 import unittest
 
-from src.logistic_func import LogisticFunction
+from src.logistic_func import calculate_payment
 
 
 class TestLogisticFunction(unittest.TestCase):
-    def setUp(self):
-        self.logistic_fn = LogisticFunction()
 
     def test_calculate_below_50(self):
-        self.assertEqual(9000, self.logistic_fn.calculate_payment(25))
-        self.assertEqual(9800, self.logistic_fn.calculate_payment(30))
-        self.assertEqual(11400, self.logistic_fn.calculate_payment(40))
-        self.assertEqual(12840, self.logistic_fn.calculate_payment(49))
+        self.assertEqual(9000, calculate_payment(25))
+        self.assertEqual(9800, calculate_payment(30))
+        self.assertEqual(11400, calculate_payment(40))
+        self.assertEqual(12840, calculate_payment(49))
 
     def test_calculate_50_to_59(self):
-        self.assertEqual(15000, self.logistic_fn.calculate_payment(50))
-        self.assertEqual(16000, self.logistic_fn.calculate_payment(55))
-        self.assertEqual(16800, self.logistic_fn.calculate_payment(59))
+        self.assertEqual(15000, calculate_payment(50))
+        self.assertEqual(16000, calculate_payment(55))
+        self.assertEqual(16800, calculate_payment(59))
 
     def test_calculate_60_to_69(self):
-        self.assertEqual(20500, self.logistic_fn.calculate_payment(62))
-        self.assertEqual(21250, self.logistic_fn.calculate_payment(65))
+        self.assertEqual(20500, calculate_payment(62))
+        self.assertEqual(21250, calculate_payment(65))
 
     def test_calculate_70_and_above(self):
-        self.assertEqual(40000, self.logistic_fn.calculate_payment(70))
-        self.assertEqual(45000, self.logistic_fn.calculate_payment(80))
-        self.assertEqual(50000, self.logistic_fn.calculate_payment(90))
-        self.assertEqual(55000, self.logistic_fn.calculate_payment(100))
+        self.assertEqual(40000, calculate_payment(70))
+        self.assertEqual(45000, calculate_payment(80))
+        self.assertEqual(50000, calculate_payment(90))
+        self.assertEqual(55000, calculate_payment(100))
 
     def test_that_successful_deliveries_greater_than_100_throws_exception(self):
-        self.assertRaises(ValueError, lambda: self.logistic_fn.calculate_payment(120))
+        self.assertRaises(ValueError, lambda: calculate_payment(120))
 
     def test_negative_successful_delivery_provided_throws_exception(self):
-        self.assertRaises(ValueError, lambda: self.logistic_fn.calculate_payment(-1))
+        self.assertRaises(ValueError, lambda: calculate_payment(-1))
